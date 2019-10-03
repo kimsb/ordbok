af2f68ef704bc36eecad22543d6907fce1c6f907
kimsb <hwY0HbY22Y9n>
2019-10-03 19:57:41 +0200

    Initial Commit

---
+++ ordbok/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ordbok
+//
+//  Created by Kim Stephen Bovim on 03/10/2019.
+//  Copyright © 2019 Kim Stephen Bovim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
